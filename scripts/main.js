8f2c5177e2e4a9e05dfc5fd7f54c14a416720501
magomatiassss <102184195+magomatiassss@users.noreply.github.com>
2022-03-23 20:02:01 -0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,3 @@
-let miImage = document.querySelector('img');
-miImage.onclick = function () {
-    let miSrc = miImage.getAttribute('src');
-    if (miSrc === 'images/lola.jpeg') {
-      miImage.setAttribute('src','images/lola2.jpeg');
-    } else {
-      miImage.setAttribute('src', 'images/lola.jpeg');
-    }
-}
-
 let miBoton = document.querySelector('button');
 let miTitulo = document.querySelector( 'h1');
 
@@ -23,3 +13,22 @@ function estableceNombreUsuario() {
 miBoton.onclick = function() {
     estableceNombreUsuario();
 }
+let html = document.getElementById("tiempo");
+
+setInterval(function(){
+	tiempo = new Date();
+
+	horas = tiempo.getHours();
+	minutos = tiempo.getMinutes();
+	segundos = tiempo.getSeconds();
+
+	//evitar los 0 o numeros individuales
+	if(horas<10)
+		horas = "0"+horas;
+	if(minutos<10)
+		minutos = "0"+minutos;
+	if(segundos<10)
+		segundos = "0"+segundos;
+
+	html.innerHTML = horas+" : "+minutos+" : "+segundos;
+},1000);
